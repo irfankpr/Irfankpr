9c71367343b4c078493af1fb9e58663bd735d33e
irfan <105389506+irfankpr@users.noreply.github.com>
2022-08-02 10:37:35 +0530

    with validation

---
+++ assets/js/formvalid.js
@@ -0,0 +1,61 @@
+function isname() {
+    var x = document.getElementById("fName").value;
+    var z = 0;
+    z = /\d/.test(x);
+    if (z == 0) {
+        document.getElementById("nameWarning").innerHTML = ""
+    } else {
+        document.getElementById("nameWarning").innerHTML = "Name cannot contain number !"
+
+    }
+}
+
+
+function isn() {
+    var ph = document.getElementById("phone").value;
+    var z = isNaN(ph)
+    if (z == 1) {
+        document.getElementById("phnWarning").innerHTML = "Contact must be numeric !";
+    } else {
+        document.getElementById("phnWarning").innerHTML = "";
+    }
+}
+
+function isTen() {
+    var x = document.getElementById("phone").value;
+    var len = x.length;
+    if (len == 0) {
+        document.getElementById("phnWarning").innerHTML = "";
+    } else if (len < 10 || len > 10) {
+        document.getElementById("phnWarning").innerHTML = "Contact number must be 10 digit.";
+    } else {
+        document.getElementById("phnWarning").innerHTML = "";
+    }
+}
+
+function isMail() {
+    var x = document.getElementById("mail").value;
+    var len = x.length;
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(x) || len == 0) {
+        document.getElementById("mWarning").innerHTML = "";
+    } else {
+        document.getElementById("mWarning").innerHTML = "Enter a valid e mail";
+    }
+}
+
+function isempty(){
+    var x= document.getElementById("subject").value.length;
+    var m= document.getElementById("message").value;
+
+    if(x==0){
+        document.getElementById("subWarning").innerHTML = "Please enter the subject";
+    }else{
+        document.getElementById("subWarning").innerHTML = "";
+    }
+
+    if(m==0){
+        document.getElementById("msgWarning").innerHTML = "It's better to add a massage. Even though you can submit without one.";
+    }else{
+        document.getElementById("msgWarning").innerHTML = "";
+    }
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -165,42 +165,7 @@
   }
 
   /**
-   * Clients Slider
-   */
-  new Swiper('.clients-slider', {
-    speed: 400,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 40
-      },
-      480: {
-        slidesPerView: 3,
-        spaceBetween: 60
-      },
-      640: {
-        slidesPerView: 4,
-        spaceBetween: 80
-      },
-      992: {
-        slidesPerView: 6,
-        spaceBetween: 120
-      }
-    }
-  });
-
-  /**
+   /**
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
@@ -255,24 +220,7 @@
   });
 
   /**
-   * Testimonials slider
-   */
-  new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
-  });
 
-  /**
    * Animation on scroll
    */
   window.addEventListener('load', () => {
